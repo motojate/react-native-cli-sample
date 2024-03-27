4f4935dedee1bb3fe18a34452682299d79cd37e6
motojate <motojate@naver.com>
2024-03-27 23:33:16 +0900

    * native 프로젝트 init

--- App.tsx
+++ App.tsx
@@ -5,114 +5,77 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import React, {useRef} from 'react';
+import {Button} from 'react-native';
+import AppleHealthKit, {HealthKitPermissions} from 'react-native-health';
+import WebView from 'react-native-webview';
+import {WebViewMessageEvent} from 'react-native-webview/lib/WebViewTypes';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const webViewRef = useRef<WebView>(null);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const sendMessageToWeb = () => {
+    const sendData = JSON.stringify({
+      id: 1,
+      type: '',
+      name: 'ssilook',
+      content: 'WebView_Test',
+    });
+
+    webViewRef.current?.postMessage(sendData);
+    console.log(webViewRef.current?.context);
+  };
+
+  const permissions = {
+    permissions: {
+      read: [
+        AppleHealthKit.Constants.Permissions.HeartRate,
+        AppleHealthKit.Constants.Permissions.Steps,
+      ],
+      write: [AppleHealthKit.Constants.Permissions.Steps],
+    },
+  } as HealthKitPermissions;
+
+  AppleHealthKit.initHealthKit(permissions, (error: string) => {
+    /* Called after we receive a response from the system */
+
+    if (error) {
+      console.log('[ERROR] Cannot grant permissions!');
+    }
+
+    /* Can now read or write to HealthKit */
+
+    // let startDate = new Date(2022, 1, 1).toString(); // 예시 시작 날짜
+    // let endDate = new Date().toString(); // 현재 날짜
+
+    AppleHealthKit.getStepCount(
+      {date: '2024-03-24'},
+      (err: Object, results: any) => {
+        if (err) {
+          return;
+        }
+
+        console.log(results);
+        webViewRef.current?.postMessage(results);
+      },
+    );
+  });
+
+  const onMessage = (event: WebViewMessageEvent) => {
+    console.log(event.nativeEvent.data);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <>
+      <WebView
+        originWhitelist={['*']}
+        source={{uri: 'http://localhost:8082'}}
+        ref={webViewRef}
+        onMessage={onMessage}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Button title="Send Message to Web" onPress={sendMessageToWeb} />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
